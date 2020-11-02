4cffb118a375d98b051b4829ac073ed15f3eaea1
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-02 19:16:22 +0600

    Added update and delete functionality

--- tasks/views.py
+++ tasks/views.py
@@ -7,7 +7,7 @@ from .forms import *
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by("-id")
 
     form = TaskForm()
 
@@ -19,3 +19,30 @@ def index(request):
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+    # name = Task.objects.get()
+    task = Task.objects.get(id = pk)
+
+    form = TaskForm(instance = task)
+
+    if request.method == 'POST':
+        #returnign the same instance. previously in index it would create a new instance
+        form = TaskForm(request.POST, instance = task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id = pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'tasks/delete.html', context)
